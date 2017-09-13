438a6c530ed34ef9045495bc7c53a542a718214f
Minki <cmk330@gmail.com>
2017-09-13 20:47:26 +0900

    Step2: Connect the UI to Code

---
+++ FoodTrackerStudy/FoodTrackerStudy/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  FoodTrackerStudy
+//
+//  Created by Minki on 2017. 9. 9..
+//  Copyright © 2017년 Minki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var mealLabel: UILabel!
+    @IBOutlet weak var mealTextField: UITextField!
+    
+    let defaultText = "음식 이름"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        mealTextField.delegate = self
+        
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        mealTextField.resignFirstResponder()
+        mealTextField.text = ""
+
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        mealLabel.text = mealTextField.text
+    }
+
+    @IBAction func setDefaultButton(_ sender: UIButton) {
+        mealLabel.text = defaultText
+    }
+}
+
