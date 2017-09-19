c35887f4fd8c41f6903a1d749c64cfc8815e1276
indam944 <na940107@naver.com>
2017-09-19 16:42:45 +0900

    BabyDiary Project create And Storyboard design

---
+++ BabyDiary/BabyDiary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BabyDiary
+//
+//  Created by INDAM94 on 2017. 9. 19..
+//  Copyright © 2017년 DGE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
