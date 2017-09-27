3e72101d27f322492840bf1d9fc474f17fb627bb
ekvlsp819 <ekvlsp819@gmail.com>
2017-09-28 02:32:43 +0900

    Revert "스토리보드 삭제후 런처스토리에서 작업, 삭제후 새프로젝트...."
    This reverts commit fe875fbe7b7e1a10a893cc61ead747ca55ab4ce0.

---
+++ project/project/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  project
+//
+//  Created by 박수연 on 2017. 9. 21..
+//  Copyright © 2017년 박수연. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var mainview: UIStackView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
