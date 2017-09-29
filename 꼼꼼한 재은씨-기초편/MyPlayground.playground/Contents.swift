10f3bb6c273aec5a6d388def1f5686dd38caec8e
ekvlsp819 <ekvlsp819@gmail.com>
2017-09-29 15:50:41 +0900

    기초편 소스 정리-기초 문법

---
+++ 꼼꼼한 재은씨-기초편/MyPlayground.playground/Contents.swift
@@ -0,0 +1,98 @@
+
+/*
+//변수
+var vValue = 3
+vValue = 7
+
+//상수
+var cValue = 3
+cValue = 7
+*/
+
+
+/*
+//변수의 선언및 초기화
+var intValue = 3//int타입으로 초기화
+var strValue = "Hello world"//string타입으로 초기화
+
+//새로운 타입으로 변환
+intValue = "안녕하세요"
+strValue = 4
+
+*/
+
+
+/*
+//타입 추론과 타입 어노테이션
+//선언과  초기화를 분리
+var year //선언부분
+year = 1999//초기화 부분
+
+
+//var year : int //선언
+year = 1999//초기화 부분
+
+*/
+
+/*
+//다른 변수끼리의 결합
+
+
+var stmt = "꼼꼼한 재은씨의 키는"
+var height = 185
+
+var heughtstmt = stmt + String(height)
+
+*/
+
+/*
+//문자열 팜플릿
+//상수와 변수를 정의
+let name = "꼼꼼한 재은씨"
+let year = 2014
+let month = 10
+let day = 1
+
+//사용법
+let profile = "\(name)는 \(year)년 \(month)월 \(day)일에 출시된다"
+print(profile)
+*/
+
+
+/*
+
+//범위 연산자의 활용
+let a = 1
+let b = 5
+
+//닫힌 범위 연산자
+for row in a...b {
+    row
+}
+
+// 반닫힌 범위 연산자
+for row in a..<b{
+    row
+}
+
+ 
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
